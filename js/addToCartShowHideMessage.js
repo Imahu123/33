d2a19d4e780a147dfcebd5c9931845e8d101714a
Imahu123 <81430337+Imahu123@users.noreply.github.com>
2021-04-07 17:08:33 +0400

    1

---
+++ js/addToCartShowHideMessage.js
@@ -0,0 +1,4 @@
+﻿var animated=false;$(function(){messageOnSave=function($button){if(animated==true){return false;}
+if($button.data('hasextras')=='1'){return;}
+var id=$button.data('itemid');var $element=$('.addToBag[data-itemid='+id+']');if($element.length<=0){$element=$('.addToBag');}
+animated=true;$element.addClass('added');setTimeout(function(){animated=false;$element.removeClass('added');},1500);}});
